0b0ea7c05a30432597d98119e02b570742c23420
2984437518 <127410382+2984437518@users.noreply.github.com>
2023-06-23 13:11:21 +0800

    file

---
+++ index.js
@@ -0,0 +1,24 @@
+function ti(){
+let user = document.getElementById("user").value;
+let pass = document.getElementById("pass").value;
+if(user=="377185524"||pass=="00zhang123"){
+alert("您已登录管理员账号");
+window.open("hello.html")
+}
+if(user=="366145524"||pass=="00123456"){
+alert("登录成功");
+window.open("hello.html")
+}
+if(user=="388543524"||pass=="00885288"){
+alert("登录成功");
+window.open("hello.html")
+}
+if(user=="399463758"||pass=="00951680"){
+alert("登录成功");
+window.open("hello.html")
+}
+if(user=="378854325"||pass=="008854369"){
+alert("登录成功");
+window.open("hello.html")
+}
+}
