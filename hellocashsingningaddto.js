b2b013531041e3d42ea6b57cc6a0aad31395ca71
2984437518 <2984437518@qq.com>
2023-08-01 12:35:06 +0800

    CancerAi-upload

---
+++ hellocashsingningaddto.js
@@ -0,0 +1,220 @@
+function sheng(){
+ let put = document.getElementById("put").value;
+ let puty = document.getElementById("puty").value;
+ let pu = document.getElementById("pu").value;
+ 
+if(puty == "1" && pu == "是"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='con'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='sybs'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='co'></p><hr/><br>"
+var bit = [
+"http://test.cashbook.work/42f5e632cb174e65a33273ef4e7389f8",
+"http://test.cashbook.work/316ec0e69c7344beb6990c37bbabd6a1",
+"http://test.cashbook.work/4f4d5c0f676e4f8d839d8de9369331ab",
+"http://test.cashbook.work/05fddcacbdec492c993f74828f138d94",
+"http://test.cashbook.work/2bbfa846baa04ad7abdcbae0e8fd95bf",
+"http://test.cashbook.work/ab374f6ac0a949f5a5660d42c968f746",
+"http://test.cashbook.work/37af1207b66d4159bccf3bb937814bbe"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("sybs").value = randomElement;
+con.innerHTML=put
+co.innerHTML=pu
+}
+
+if(puty == "2" && pu == "是"){
+ guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='conl'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='sybss'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='com'></p><hr/><br>"
+ 
+ var bit = [
+"http://test.cashbook.work/4679e094d9184c0a8a71b7c1fe58b571",
+"http://test.cashbook.work/9837e018e5df4f65ac0e04bb63179782",
+"http://test.cashbook.work/c1b84f66fa5e4be3b26e86f165e4087c",
+"http://test.cashbook.work/08ed41b68382438fa32f39f2ad5cffb7",
+"http://test.cashbook.work/53c7a7b22b3b438bbf5e2f3dbdbd63ec",
+"http://test.cashbook.work/5d135972b9264516accbe4de3374cd22",
+"http://test.cashbook.work/6cbd1872e6e94559959d2d04c9584556"
+
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("sybss").value = randomElement;
+conl.innerHTML=put
+com.innerHTML=pu
+}
+if(puty == "3" && pu == "是"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='conk'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='sybsss'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='coo'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/bf86b53cf9604b0bb99d1524ad3315cd",
+"http://test.cashbook.work/0b61d712677b4162ace34090d3061690",
+"http://test.cashbook.work/9f85869de3624cf7bfa04dcb00f0cdbd",
+"http://test.cashbook.work/6dfd092777704d25b14f617434eb0e61",
+"http://test.cashbook.work/b0bb8d5965874f00a2bc5abb8d0ace6c"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("sybsss").value = randomElement;
+conk.innerHTML=put
+coo.innerHTML=pu
+}
+if(puty == "4" && pu == "是"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='congg'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='sybks'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='cos'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/b911dabbba734610923d324590ebec8a",
+"http://test.cashbook.work/cab6a5360c124ae6830625a80e301f83",
+"http://test.cashbook.work/f14846a9d9524889b9de479b8f791615",
+"http://test.cashbook.work/fe66a425724d44c3a0c66645b1de9892",
+"http://test.cashbook.work/961aeef85948469588c1ce66a23b742e"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("sybks").value = randomElement;  
+congg.innerHTML=put
+cos.innerHTML=pu
+}
+if(puty == "5" && pu == "是"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='cong'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='syblllks'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='copls'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/899a2637eeee4d6b9f81e5c222e14d6d",
+"http://test.cashbook.work/adb05977008d4368a2958f0570805bfd",
+"http://test.cashbook.work/055bb01e89e7412db5a3f60b4652910e",
+"http://test.cashbook.work/b2c208b1b1bf40fbbbad82a412d6ab7e",
+"http://test.cashbook.work/6d4881a7237d40caa2419a598d28c6e4",
+"http://test.cashbook.work/c2d0b9231df342c8b86ccebf9aa05ab9",
+"http://test.cashbook.work/20ba732e31574e1a9ba0842b0a08bf58",
+"http://test.cashbook.work/afc80a64cafa444b8bfd1badae3d56dd",
+"http://test.cashbook.work/9fb2245610c54f7da4b1ed69cf7659a3"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("syblllks").value = randomElement;  
+cong.innerHTML=put
+copls.innerHTML=pu
+}
+if(puty == "1" && pu == "否"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='conb'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='ybs'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='coj'></p><hr/><br>"
+
+ var bit = [
+"http://test.cashbook.work/42f5e632cb174e65a33273ef4e7389f8",
+"http://test.cashbook.work/316ec0e69c7344beb6990c37bbabd6a1",
+"http://test.cashbook.work/4f4d5c0f676e4f8d839d8de9369331ab",
+"http://test.cashbook.work/05fddcacbdec492c993f74828f138d94",
+"http://test.cashbook.work/2bbfa846baa04ad7abdcbae0e8fd95bf",
+"http://test.cashbook.work/ab374f6ac0a949f5a5660d42c968f746",
+"http://test.cashbook.work/37af1207b66d4159bccf3bb937814bbe"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("ybs").value = randomElement;
+ conb.innerHTML=put  
+  coj.innerHTML=pu
+}
+if(puty == "2" && pu == "否"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='coy'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='sbs'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='cov'></p><hr/><br>"
+
+var bit = [
+
+ "http://test.cashbook.work/4679e094d9184c0a8a71b7c1fe58b571",
+"http://test.cashbook.work/9837e018e5df4f65ac0e04bb63179782",
+"http://test.cashbook.work/c1b84f66fa5e4be3b26e86f165e4087c",
+"http://test.cashbook.work/08ed41b68382438fa32f39f2ad5cffb7",
+"http://test.cashbook.work/53c7a7b22b3b438bbf5e2f3dbdbd63ec",
+"http://test.cashbook.work/5d135972b9264516accbe4de3374cd22",
+"http://test.cashbook.work/6cbd1872e6e94559959d2d04c9584556"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("sbs").value = randomElement;
+   coy.innerHTML=put
+   cov.innerHTML=pu
+}
+if(puty == "3" && pu == "否"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='coh'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='bs'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='coa'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/bf86b53cf9604b0bb99d1524ad3315cd",
+"http://test.cashbook.work/0b61d712677b4162ace34090d3061690",
+"http://test.cashbook.work/9f85869de3624cf7bfa04dcb00f0cdbd",
+"http://test.cashbook.work/6dfd092777704d25b14f617434eb0e61",
+"http://test.cashbook.work/b0bb8d5965874f00a2bc5abb8d0ace6c"
+  ]
+  var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("bs").value = randomElement;
+   coh.innerHTML=put
+   coa.innerHTML=pu
+}
+if(puty == "4" && pu == "否"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='coxxx'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='webs'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='copop'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/b911dabbba734610923d324590ebec8a",
+"http://test.cashbook.work/cab6a5360c124ae6830625a80e301f83",
+"http://test.cashbook.work/f14846a9d9524889b9de479b8f791615",
+"http://test.cashbook.work/fe66a425724d44c3a0c66645b1de9892",
+"http://test.cashbook.work/961aeef85948469588c1ce66a23b742e"
+  ]
+  var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("webs").value = randomElement;
+   coxxx.innerHTML=put
+   copop.innerHTML=pu
+} 
+if(puty == "5" && pu == "否"){
+  guan.innerHTML+="&nbsp;&nbsp;&nbsp;编号:<p id='congbh'>0</p> <hr/> <p>生成的链接:</p>  &nbsp;&nbsp;&nbsp;<textarea id='syblllks'name='message' cols='50'rows='5'></textarea>     <hr/>  <p>是否防封:</p><p id='commls'></p><hr/><br>"
+
+var bit = [
+"http://test.cashbook.work/899a2637eeee4d6b9f81e5c222e14d6d",
+"http://test.cashbook.work/adb05977008d4368a2958f0570805bfd",
+"http://test.cashbook.work/055bb01e89e7412db5a3f60b4652910e",
+"http://test.cashbook.work/b2c208b1b1bf40fbbbad82a412d6ab7e",
+"http://test.cashbook.work/6d4881a7237d40caa2419a598d28c6e4",
+"http://test.cashbook.work/c2d0b9231df342c8b86ccebf9aa05ab9",
+"http://test.cashbook.work/20ba732e31574e1a9ba0842b0a08bf58",
+"http://test.cashbook.work/afc80a64cafa444b8bfd1badae3d56dd",
+"http://test.cashbook.work/9fb2245610c54f7da4b1ed69cf7659a3"
+  ]
+  
+
+			
+		var randomIndex = Math.floor(Math.random() * bit.length);
+			var randomElement = bit[randomIndex];
+			document.getElementById("syblllks").value = randomElement;  
+congbh.innerHTML=put
+commls.innerHTML=pu
+} 
+}
+function a(){
+var str = document.getElementById("str");
+str.style.display = "none";
+var guan = document.getElementById("guan");
+guan.style.display = "block";
+}
+function b(){
+var str = document.getElementById("str");
+str.style.display = "block";
+var guan = document.getElementById("guan");
+guan.style.display = "none";
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+function ti() {
+    let _0x575a6b = document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0042\u0079\u0049\u0064"]("resu".split("").reverse().join(""))["\u0076\u0061\u006c\u0075\u0065"];
+    let _0x23592b = document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0042\u0079\u0049\u0064"]("\u0070\u0061\u0073\u0073")["\u0076\u0061\u006c\u0075\u0065"];
+    if (_0x575a6b == "\u0033\u0037\u0037\u0031\u0038\u0035\u0035\u0032\u0034" && _0x23592b == "321gnahz00".split("").reverse().join("")) {
+        alert("\u53F7\u8D26\u5458\u7406\u7BA1\u5F55\u767B\u5DF2\u60A8".split("").reverse().join(""));
+        window["\u006f\u0070\u0065\u006e"]("lmth.gningnisxednihsac".split("").reverse().join(""));
+    }
+    if (_0x575a6b == "423764463".split("").reverse().join("") && _0x23592b == '00949736') {
+        alert('登录成功');
+        window["\u006f\u0070\u0065\u006e"]('cashindexsingning.html');
+    }
+    if (_0x575a6b == "\u0033\u0038\u0035\u0036\u0038\u0034\u0036\u0035\u0034" && _0x23592b == "69006340".split("").reverse().join("")) {
+        alert("\u529F\u6210\u5F55\u767B".split("").reverse().join(""));
+        window["\u006f\u0070\u0065\u006e"]("lmth.gningnisxednihsac".split("").reverse().join(""));
+    }
+    if (_0x575a6b == '346431558' && _0x23592b == "346953400".split("").reverse().join("")) {
+        alert("\u529F\u6210\u5F55\u767B".split("").reverse().join(""));
+        window['open']('cashindexsingning.html');
+    }
+    if (_0x575a6b == "736673173".split("").reverse().join("") && _0x23592b == '009563759') {
+        alert("\u529F\u6210\u5F55\u767B".split("").reverse().join(""));
+        window['open']("\u0063\u0061\u0073\u0068\u0069\u006e\u0064\u0065\u0078\u0073\u0069\u006e\u0067\u006e\u0069\u006e\u0067\u002e\u0068\u0074\u006d\u006c");
+    }
+    if (_0x575a6b == "\u0033\u0030\u0031\u0031\u0039\u0037\u0036\u0035\u0030\u0036" && _0x23592b == "0254131eeA".split("").reverse().join("")) {
+        alert("\u767b\u5f55\u6210\u529f");
+        window['open']('cashindexsingning.html');
+    }
+}
